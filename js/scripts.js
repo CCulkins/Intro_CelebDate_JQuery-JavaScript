c15c3ea415a670d8c41c439f1852d457bbf90aac
Cassandra Culkins and Chris Kuiper <cassy.culkins@gmail.com>
2016-01-13 11:20:55 -0800

    add content to html with js selectors

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form#celeb").submit(function(event) {
+    var age = ("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age) {
+    var match = (<30, >18);
+    if (gender === 'male' && age < 30) {
+      celeb-match = ;
+    }
+
+
+}
