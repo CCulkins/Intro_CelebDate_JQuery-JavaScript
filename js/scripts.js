d0e16c704126be7f49a7447b089a809a755f6c35
Cassandra Culkins and Chris Kuiper <chris.kuiper@gmail.com>
2016-01-13 12:47:04 -0800

    correctly display matching criteria with variale

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
   $("form#celeb").submit(function(event) {
-    var age = ("input#age").val());
-    var gender = $("select#gender").val();
+    var ageInput = $("input#age").val();
+    var genderInput = $("select#gender").val();
 
-    if (age) {
-    var match = (<30, >18);
-    if (gender === 'male' && age < 30) {
-      celeb-match = ;
+    if (ageInput <=30 && genderInput=== 'male') {
+       var celebmatch = "Liam Hemsworth";
+    }
+    if (ageInput >30 && genderInput=== 'male') {
+       var celebmatch = "Tatem Channing";
+    }
+    if (ageInput >=40 && genderInput=== 'male') {
+       var celebmatch = "Bruce Willis";
+    }
+    if (ageInput <=30 && genderInput=== 'female') {
+       var celebmatch = "Jennifer Lawrence";
+    }
+    if (ageInput >30 && genderInput=== 'female') {
+       var celebmatch = "Reese Witherspoon";
+    }
+    if (ageInput >=40 && genderInput=== 'female') {
+       var celebmatch = "Michelle Pfeiffer";
     }
 
+    $("span#match").text(celebmatch);
+    $("#celebmatch").show();
 
-}
+    event.preventDefault();
+  });
+});
